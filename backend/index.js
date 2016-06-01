07384cee0b483c2ca7e432a338e3898aba0f4641
BransonGitomeh <sirbranson@outlook.com>
2016-06-02 01:37:17 +0300

    added body parsing so express can pick the body inside post

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express()
 var graphql = require("graphql")
 var schema = require("./graphSchema")
-
+var bodyParser  = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send("welcome to projectExpresso");
@@ -17,8 +19,12 @@ app.post('/graph', function (req, res) {
   var rangeQuery = '{ contacts { id } }';
 
   var createContactMutation = 'mutation { create { contact(name:"Branson Gitomeh") { name } } }';
+  
+  console.log(req.body.query)
+  
+  const query = req.body.query
 
-  graphql.graphql(schema, createContactMutation).then(result => {
+  graphql.graphql(schema, query).then(result => {
     console.log(result);
     res.send(result)
   });
