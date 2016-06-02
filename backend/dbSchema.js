161cfb96f903220f758e62ebb72a582db5e5cb1d
BransonGitomeh <sirbranson@outlook.com>
2016-06-02 17:15:02 +0300

    added a real hosted mongo instance, and defaulted db to mongo

--- backend/dbSchema.js
+++ backend/dbSchema.js
@@ -34,11 +34,7 @@ var config = {
         },
         mongo: {
             adapter: 'mongo',
-            host: 'localhost', // defaults to `localhost` if omitted
-            port: 27017, // defaults to 27017 if omitted
-            user: 'username_here', // or omit if not relevant
-            password: 'password_here', // or omit if not relevant
-            database: 'database_name_here' // or omit if not relevant
+            url:"mongodb://kamikazechaser:kamikazechaser@ds013221.mlab.com:13221/expresso"
         }
     }
 }
